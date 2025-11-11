a3d4c903f0514b15df540ba28a05563dfe4554d3
Mohammadshadathusen <shadatrolli05@gmail.com>
2025-11-11 18:18:51 +0530

    Added cosine similarity function

--- linear_algebra.py
+++ linear_algebra.py
@@ -13,15 +13,42 @@ def inf_norm(v):
 def norm(v):
     """Day 2 of implementing vector norm Eucledian norm"""
     return math.sqrt(sum(abs(x)*abs(x) for x in v))
+def cosine(a, b):
+    """Implementing cosine similarity"""
+    dotprdct=0
+    for a1,b1 in zip(a,b):
+        dotprdct+=a1*b1
+    #||a|| and ||b||
+    x=math.sqrt(sum(i*i for i in a))
+    y=math.sqrt(sum(i*i for i in b))
+    final=dotprdct/(x*y)
+    return final
+    
+
+    
+        
+
+    
+
 
 
 
 if __name__== "__main__":
     #DAy 1
-    #creating l1 ,l2 ,inf_normobjects to verify
-    # print(l1_norm([3, -4, 2]))   # 9
-    # print(l2_norm([3, 4]))       # 5
-    # print(inf_norm([3, -4, 2]))  # 4
+    # creating l1 ,l2 ,inf_normobjects to verify
+    print(l1_norm([3, -4, 2]))   # 9
+    print(l2_norm([3, 4]))       # 5
+    print(inf_norm([3, -4, 2]))  # 4
+
+
+    #Day 2
+
+    #Eucledian norm: testing
     print(norm([3,4]))  # should print 5
+
+    #Cosine similarity
+    print(cosine([2,3,1],[1,0,-1]))
+
+
 
     
